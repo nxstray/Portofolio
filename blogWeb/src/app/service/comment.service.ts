04c4fe19a30116262a064311b8ebe08e895eb4a4
nxstray <afwanapriansyah@gmail.com>
2025-06-18 05:14:17 +0700

    module problems: solved

--- blogWeb/src/app/service/comment.service.ts
+++ blogWeb/src/app/service/comment.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+const BASIC_URL = 'http://localhost:9090/';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommentService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  createComment(postId:number, postedBy:string, content:string) : Observable<any> {
+    const params = {
+      postId: postId,
+      postedBy: postedBy,
+      content: content
+    }
+    return this.http.post(BASIC_URL + `api/comments/create`, content, { params });
+  }
 }
