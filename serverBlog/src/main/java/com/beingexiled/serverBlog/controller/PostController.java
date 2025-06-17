5ea1f3669f695ccec1a0ca7207d20a4636ce8d57
nxstray <afwanapriansyah@gmail.com>
2025-06-18 01:18:24 +0700

    updating viewCount: done

--- serverBlog/src/main/java/com/beingexiled/serverBlog/controller/PostController.java
+++ serverBlog/src/main/java/com/beingexiled/serverBlog/controller/PostController.java
@@ -11,6 +11,9 @@ import com.beingexiled.serverBlog.service.PostService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import jakarta.persistence.EntityNotFoundException;
+
 import java.util.List;
 
 @RestController
@@ -37,6 +40,16 @@ public class PostController {
             return ResponseEntity.status(HttpStatus.OK).body(postService.getAllPosts());
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @GetMapping("/{postId}")
+    public ResponseEntity<?> getPostById(@PathVariable Long postId) {
+        try {
+            Post post = postService.getPostById(postId);
+            return ResponseEntity.status(HttpStatus.OK).body(post);
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
         }
     }
 }
