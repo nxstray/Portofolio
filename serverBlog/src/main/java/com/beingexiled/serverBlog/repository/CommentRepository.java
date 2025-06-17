8368b8c40718d92f9022f4c6416389e46d128529
nxstray <afwanapriansyah@gmail.com>
2025-06-18 05:37:20 +0700

    updating for list comments: done

--- serverBlog/src/main/java/com/beingexiled/serverBlog/repository/CommentRepository.java
+++ serverBlog/src/main/java/com/beingexiled/serverBlog/repository/CommentRepository.java
@@ -4,7 +4,10 @@ import com.beingexiled.serverBlog.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    // Custom query methods only (if needed)
+    
+    List<Comment> findByPostId(Long postId);
 }
