2f0b60227275f6919bfc1b8a3850f384759d5bbd
nxstray <afwanapriansyah@gmail.com>
2025-06-18 05:35:36 +0700

    updating Getmapping for GET comments & endpoint problems: solved

--- serverBlog/src/main/java/com/beingexiled/serverBlog/controller/CommentController.java
+++ serverBlog/src/main/java/com/beingexiled/serverBlog/controller/CommentController.java
@@ -2,6 +2,8 @@ package com.beingexiled.serverBlog.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,6 +27,15 @@ public class CommentController {
             return ResponseEntity.ok(commentService.createComment(postId, postedBy, content));
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error creating comment: " + e.getMessage());
+        }
+    }
+
+    @GetMapping("/{postId}")
+    public ResponseEntity<?> getCommentsByPostId(@PathVariable Long postId) {
+        try {
+            return ResponseEntity.ok(commentService.getCommentsByPostId(postId));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error fetching comments: " + e.getMessage());
         }
     }
 }
