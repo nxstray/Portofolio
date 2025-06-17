e7e63c8221111d26884f2d751773ee266823fcae
nxstray <afwanapriansyah@gmail.com>
2025-06-18 02:10:54 +0700

    adding view-all comp: done

--- blogWeb/src/app/pages/view-all/view-all.component.ts
+++ blogWeb/src/app/pages/view-all/view-all.component.ts
@@ -2,15 +2,30 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PostService } from '../../service/post.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-view-all',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatGridListModule,
+    MatIconModule,
+    MatButtonModule,
+    RouterModule
+  ],
   templateUrl: './view-all.component.html',
   styleUrls: ['./view-all.component.scss']
 })
 export class ViewAllComponent {
+
+  allPosts:any[] = [];
+
   constructor(
     private postService: PostService,
     private snackBar: MatSnackBar
@@ -24,6 +39,7 @@ export class ViewAllComponent {
     this.postService.getAllPosts().subscribe({
       next: (res) => {
         console.log(res);
+        this.allPosts = res;
       },
       error: () => {
         this.snackBar.open("Error while fetching posts", "Close");
