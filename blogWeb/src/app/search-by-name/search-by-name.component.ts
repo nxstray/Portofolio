76e2353c0dc5c8e3375d26daa56f3cb6a9335ad2
nxstray <afwanapriansyah@gmail.com>
2025-06-19 01:14:06 +0700

    adding new component: done

---
+++ blogWeb/src/app/search-by-name/search-by-name.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-by-name',
+  imports: [],
+  templateUrl: './search-by-name.component.html',
+  styleUrl: './search-by-name.component.scss'
+})
+export class SearchByNameComponent {
+
+}
