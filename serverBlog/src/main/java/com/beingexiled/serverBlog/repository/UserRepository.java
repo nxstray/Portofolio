e338e8f8c4a973551a90d08a8ce5c9383dde4781
nxstray <afwanapriansyah@gmail.com>
2025-06-19 02:31:01 +0700

    adding + updating user repo: done

---
+++ serverBlog/src/main/java/com/beingexiled/serverBlog/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.beingexiled.serverBlog.repository;
+
+import com.beingexiled.serverBlog.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
