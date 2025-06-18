8f199d8d8596c514db1224aa050b0b7a381bc5cc
nxstray <afwanapriansyah@gmail.com>
2025-06-19 02:40:04 +0700

    token problems: done

--- serverBlog/src/main/java/com/beingexiled/serverBlog/security/JwtUtil.java
+++ serverBlog/src/main/java/com/beingexiled/serverBlog/security/JwtUtil.java
@@ -1,5 +1,35 @@
 package com.beingexiled.serverBlog.security;
 
+import io.jsonwebtoken.*;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
 public class JwtUtil {
-    
+
+    private final String jwtSecret = "your_jwt_secret_key";
+    private final long jwtExpirationMs = 86400000;
+
+    public String generateToken(String email) {
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(new Date().getTime() + jwtExpirationMs))
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .compact();
+    }
+
+    public String getEmailFromToken(String token) {
+        return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
 }
