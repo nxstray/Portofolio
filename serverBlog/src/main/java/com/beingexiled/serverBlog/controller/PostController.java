3d0923c967739efe80dd9aa0f0a45189c26e5165
nxstray <afwanapriansyah@gmail.com>
2025-06-19 04:35:17 +0700

    protection for PUT & DELETE problems: solved

--- serverBlog/src/main/java/com/beingexiled/serverBlog/controller/PostController.java
+++ serverBlog/src/main/java/com/beingexiled/serverBlog/controller/PostController.java
@@ -4,11 +4,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestBody;
 import com.beingexiled.serverBlog.entity.Post;
 import com.beingexiled.serverBlog.service.PostService;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -60,5 +63,20 @@ public class PostController {
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
+    }
+
+    @PutMapping("/{postId}")
+    public ResponseEntity<?> updatePost(@PathVariable Long postId, @RequestBody Post postDetails) {
+        Post post = postService.getPostById(postId);
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String email = authentication.getName();
+        if (post.getUser() != null && !post.getUser().getEmail().equals(email)) {
+            throw new SecurityException("You are not authorized to create this post.");
+        }
+
+        post.setName(postDetails.getName());
+        post.setContent(postDetails.getContent());
+        return ResponseEntity.ok(postService.savePost(post));
     }
 }
