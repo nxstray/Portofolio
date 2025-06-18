1ca07218ddb951693dcd337f6b1cc896e1ec16f3
nxstray <afwanapriansyah@gmail.com>
2025-06-19 02:31:59 +0700

    updating user: done

--- serverBlog/src/main/java/com/beingexiled/serverBlog/entity/User.java
+++ serverBlog/src/main/java/com/beingexiled/serverBlog/entity/User.java
@@ -1,5 +1,22 @@
 package com.beingexiled.serverBlog.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "users")
 public class User {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+    
+    private String role = "USER";
 }
