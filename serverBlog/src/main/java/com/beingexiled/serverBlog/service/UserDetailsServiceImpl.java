a6af62deab1c7c23b725353680f11c0a5f301eca
nxstray <afwanapriansyah@gmail.com>
2025-06-19 03:44:05 +0700

    adding + updating load user data: done

---
+++ serverBlog/src/main/java/com/beingexiled/serverBlog/service/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package com.beingexiled.serverBlog.service;
+
+import com.beingexiled.serverBlog.repository.UserRepository;
+import com.beingexiled.serverBlog.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+            .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getEmail())
+                .password(user.getPassword())
+                .roles("USER") // atau dari user.getRole()
+                .build();
+    }
+}
