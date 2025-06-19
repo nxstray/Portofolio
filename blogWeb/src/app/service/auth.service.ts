4557e9f83e990cb2bf4070f561bd4de98da98399
nxstray <afwanapriansyah@gmail.com>
2025-06-19 19:08:37 +0700

    adding + updating user auth problems: solved

---
+++ blogWeb/src/app/service/auth.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable, catchError, tap, throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private apiUrl = 'http://localhost:9090/api/auth';
+
+  private loggedIn = new BehaviorSubject<boolean>(false);
+  isLoggedIn$ = this.loggedIn.asObservable();
+
+  constructor(private http: HttpClient) {}
+
+  signIn(email: string, password: string): Observable<any> {
+    return this.http.post(`${this.apiUrl}/signin`, { email, password }, { withCredentials: true }).pipe(
+      tap(() => this.loggedIn.next(true)),
+      catchError((err) => {
+        this.loggedIn.next(false);
+        return throwError(() => err);
+      })
+    );
+  }
+
+  signUp(email: string, password: string): Observable<any> {
+    return this.http.post(`${this.apiUrl}/signup`, { email, password });
+  }
+
+  checkLoginStatus(): void {
+    this.http.get(`${this.apiUrl}/check`, { withCredentials: true }).subscribe({
+      next: () => this.loggedIn.next(true),
+      error: () => this.loggedIn.next(false),
+    });
+  }
+
+  logout(): void {
+    this.http.post(`${this.apiUrl}/logout`, {}, { withCredentials: true }).subscribe({
+      next: () => this.loggedIn.next(false),
+      error: () => this.loggedIn.next(false),
+    });
+  }
+}
