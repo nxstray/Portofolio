5192615ee08dde2f7adb47fb37664313eb415d94
nxstray <afwanapriansyah@gmail.com>
2025-06-19 19:11:21 +0700

    login & logout auth problems: solved

--- blogWeb/src/app/app.component.ts
+++ blogWeb/src/app/app.component.ts
@@ -1,17 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { AuthService } from './service/auth.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  imports: [MatToolbarModule, MatIconModule, MatButtonModule, RouterOutlet, RouterModule]
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    RouterOutlet,
+    RouterModule
+  ]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'blogWeb';
+  isLoggedIn$!: Observable<boolean>;
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.authService.checkLoginStatus();
+    this.isLoggedIn$ = this.authService.isLoggedIn$;
+  }
+
+  logout(): void {
+    this.authService.logout();
+  }
 }
