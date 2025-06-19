e6c5273d57507c2b48a04732d058c49b7ef16c4d
nxstray <afwanapriansyah@gmail.com>
2025-06-19 16:46:56 +0700

    adding uploadImage: done

--- serverBlog/src/main/java/com/beingexiled/serverBlog/service/PostService.java
+++ serverBlog/src/main/java/com/beingexiled/serverBlog/service/PostService.java
@@ -3,9 +3,13 @@ package com.beingexiled.serverBlog.service;
 import com.beingexiled.serverBlog.entity.Post;
 import java.util.List;
 
+import org.springframework.web.multipart.MultipartFile;
+
 public interface PostService {
 	
     Post savePost(Post post);
+
+    Post uploadImage(Long postId, MultipartFile imageFile);
 
     List<Post> getAllPosts();
 
