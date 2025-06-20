76daf08adc3ed535e6798110d54642286641c711
nxstray <afwanapriansyah@gmail.com>
2025-06-20 22:43:27 +0700

    adding new modules: done

--- blogWeb/src/app/app.component.ts
+++ blogWeb/src/app/app.component.ts
@@ -3,10 +3,15 @@ import { RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { AuthService } from './service/auth.service';
+import { PostService } from './service/post.service';
 import { Observable } from 'rxjs';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -19,14 +24,24 @@ import { Observable } from 'rxjs';
     MatIconModule,
     MatButtonModule,
     RouterOutlet,
-    RouterModule
+    RouterModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatListModule,
+    FormsModule
   ]
 })
 export class AppComponent implements OnInit {
   title = 'blogWeb';
   isLoggedIn$!: Observable<boolean>;
+  searchResults: any[] = [];
+  currentQuery: string = '';
 
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private postService: PostService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.authService.checkLoginStatus();
@@ -35,5 +50,43 @@ export class AppComponent implements OnInit {
 
   logout(): void {
     this.authService.logout();
+  }
+
+  onSearchChange(): void {
+    const query = this.currentQuery;
+    if (query.length > 1) {
+      this.postService.searchByName(query).subscribe({
+        next: (res: any[]) => {
+          this.searchResults = res;
+        },
+        error: (err: any) => {
+          console.error(err);
+          this.searchResults = [];
+        }
+      });
+    } else {
+      this.searchResults = [];
+    }
+  }
+
+  onSearchEnter(): void {
+    if (this.currentQuery.trim()) {
+      this.searchResults = []
+      this.router.navigate(['/view-all'], { queryParams: { q: this.currentQuery.trim() } });
+      this.currentQuery = '';
+    } else if (this.searchResults.length > 1) {
+      const matched = this.searchResults.find((r: any) =>
+        r?.name?.toLowerCase().includes(this.currentQuery.toLowerCase())
+      );
+      if (matched) {
+        this.goToPost(matched.id);
+      }
+    }
+  }
+
+  goToPost(postId: number): void {
+    this.searchResults = [];
+    this.currentQuery = '';
+    this.router.navigate(['/post', postId]);
   }
 }
