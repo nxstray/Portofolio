82ecadffbfcbd45aa388eb6fbb952cf691b15846
nxstray <afwanapriansyah@gmail.com>
2025-06-20 22:40:49 +0700

    adding view-post path: done

--- blogWeb/src/app/app-routing.module.ts
+++ blogWeb/src/app/app-routing.module.ts
@@ -3,13 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { CreatePostComponent } from './pages/create-post/create-post.component';
 import { ViewAllComponent } from './pages/view-all/view-all.component';
 import { ViewPostComponent } from './pages/view-post/view-post.component';
-import { SearchByNameComponent } from './search-by-name/search-by-name.component';
 
 const routes: Routes = [
     {path: 'create-post', component: CreatePostComponent},
     {path: 'view-all', component: ViewAllComponent},
     {path: 'view-post', component: ViewPostComponent},
-    {path: 'search-by-name', component: SearchByNameComponent}
 ];
 
 @NgModule({
