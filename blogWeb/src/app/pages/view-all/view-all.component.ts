b7a73a53be157fcdd9d30361d996abdfb6bfe7c3
nxstray <afwanapriansyah@gmail.com>
2025-06-20 22:44:58 +0700

    adding new modules: done

--- blogWeb/src/app/pages/view-all/view-all.component.ts
+++ blogWeb/src/app/pages/view-all/view-all.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PostService } from '../../service/post.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -6,7 +6,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { RouterModule } from '@angular/router';
+import { RouterModule, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-view-all',
@@ -22,27 +22,45 @@ import { RouterModule } from '@angular/router';
   templateUrl: './view-all.component.html',
   styleUrls: ['./view-all.component.scss']
 })
-export class ViewAllComponent {
+export class ViewAllComponent implements OnInit {
 
-  allPosts:any[] = [];
+  allPosts: any[] = [];
 
   constructor(
     private postService: PostService,
-    private snackBar: MatSnackBar
+    private snackBar: MatSnackBar,
+    private route: ActivatedRoute
   ) {}
 
   ngOnInit() {
-    this.getAllposts();
+    this.route.queryParams.subscribe(params => {
+      const keyword = params['q'];
+      if (keyword && keyword.trim().length > 0) {
+        this.searchPosts(keyword.trim());
+      } else {
+        this.getAllPosts();
+      }
+    });
   }
 
-  getAllposts() {
+  getAllPosts() {
     this.postService.getAllPosts().subscribe({
       next: (res) => {
-        console.log(res);
         this.allPosts = res;
       },
       error: () => {
-        this.snackBar.open("Error while fetching posts", "Close", { duration: 3000});
+        this.snackBar.open("Error while fetching posts", "Close", { duration: 3000 });
+      }
+    });
+  }
+
+  searchPosts(keyword: string) {
+    this.postService.searchByName(keyword).subscribe({
+      next: (res) => {
+        this.allPosts = res;
+      },
+      error: () => {
+        this.snackBar.open("Error while searching posts", "Close", { duration: 3000 });
       }
     });
   }
