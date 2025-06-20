d1f79587d785654f09100af299c3e9b74ef44275
nxstray <afwanapriansyah@gmail.com>
2025-06-20 22:37:34 +0700

    upadting uploadImage method: done

--- blogWeb/src/app/service/post.service.ts
+++ blogWeb/src/app/service/post.service.ts
@@ -11,20 +11,25 @@ export class PostService {
 
   constructor(private http: HttpClient) { }
 
-  createNewPost(data:any) : Observable<any> {
-    return this.http.post(BASIC_URL + `api/posts`, data);
+  createNewPost(data: any): Observable<any> {
+    return this.http.post(BASIC_URL + `api/posts`, data, { withCredentials: true });
   }
 
-  getAllPosts() : Observable<any> {
+  uploadImage(postId: number, imageFile: File): Observable<any> {
+    const formData = new FormData();
+    formData.append('image', imageFile);
+    return this.http.post(BASIC_URL + `api/posts/${postId}/upload-image`, formData, { withCredentials: true });
+  }
+
+  getAllPosts(): Observable<any> {
     return this.http.get(BASIC_URL + `api/posts`);
   }
 
-  getPostById(postId:number) : Observable<any> {
+  getPostById(postId: number): Observable<any> {
     return this.http.get(BASIC_URL + `api/posts/${postId}`);
   }
 
-  searchByName(name:string) : Observable<any> {
+  searchByName(name: string): Observable<any> {
     return this.http.get(BASIC_URL + `api/posts/search/${name}`);
   }
-
 }
