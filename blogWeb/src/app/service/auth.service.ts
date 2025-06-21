587cb05227c21d64a01787093f6b03aeac5c194e
nxstray <afwanapriansyah@gmail.com>
2025-06-22 00:12:59 +0700

    checking non-login user problems: solved

--- blogWeb/src/app/service/auth.service.ts
+++ blogWeb/src/app/service/auth.service.ts
@@ -1,6 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject, Observable, catchError, tap, throwError } from 'rxjs';
+import {
+  BehaviorSubject,
+  Observable,
+  catchError,
+  map,
+  tap,
+  throwError,
+  of,
+} from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -13,27 +21,31 @@ export class AuthService {
   constructor(private http: HttpClient) {}
 
   signIn(email: string, password: string): Observable<any> {
-    return this.http.post(`${this.apiUrl}/signin`, { email, password }, { withCredentials: true }).pipe(
-      tap(() => this.loggedIn.next(true)),
-      catchError((err) => {
-        this.loggedIn.next(false);
-        return throwError(() => err);
-      })
-    );
+    return this.http
+      .post(`${this.apiUrl}/signin`, { email, password }, { withCredentials: true })
+      .pipe(
+        tap(() => this.loggedIn.next(true)),
+        catchError((err) => {
+          this.loggedIn.next(false);
+          return throwError(() => err);
+        })
+      );
   }
 
   signUp(email: string, password: string): Observable<any> {
-    return this.http.post(`${this.apiUrl}/signup`, { email, password }, { withCredentials: true }).pipe(
-      tap(() => this.loggedIn.next(true)),
-      catchError((err) => {
-        this.loggedIn.next(false);
-        return throwError(() => err);
-      })
-    );
+    return this.http
+      .post(`${this.apiUrl}/signup`, { email, password }, { withCredentials: true })
+      .pipe(
+        tap(() => this.loggedIn.next(true)),
+        catchError((err) => {
+          this.loggedIn.next(false);
+          return throwError(() => err);
+        })
+      );
   }
 
-  resetPassword(email: string): Observable<any> {
-    return this.http.post(`${this.apiUrl}/reset-password`, { email }, { withCredentials: true });
+  resetPassword(emailOrUsername: string): Observable<any> {
+    return this.http.post(`${this.apiUrl}/forgot-password`, { emailOrUsername }, { withCredentials: true });
   }
 
   checkLoginStatus(): void {
@@ -43,10 +55,27 @@ export class AuthService {
     });
   }
 
+  checkLoginStatusWithResponse(): Observable<boolean> {
+    return this.http.get(`${this.apiUrl}/check`, { withCredentials: true }).pipe(
+      map(() => {
+        this.loggedIn.next(true);
+        return true;
+      }),
+      catchError(() => {
+        this.loggedIn.next(false);
+        return of(false);
+      })
+    );
+  }
+
   logout(): void {
     this.http.post(`${this.apiUrl}/logout`, {}, { withCredentials: true }).subscribe({
       next: () => this.loggedIn.next(false),
       error: () => this.loggedIn.next(false),
     });
+  }
+
+  isLoggedIn(): boolean {
+    return this.loggedIn.value;
   }
 }
