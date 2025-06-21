03d813407974496ce44434d20f555294fd2e5beb
nxstray <afwanapriansyah@gmail.com>
2025-06-22 00:15:44 +0700

    adding new method for reset token: done

--- serverBlog/src/main/java/com/beingexiled/serverBlog/repository/UserRepository.java
+++ serverBlog/src/main/java/com/beingexiled/serverBlog/repository/UserRepository.java
@@ -1,7 +1,6 @@
 package com.beingexiled.serverBlog.repository;
 
 import com.beingexiled.serverBlog.entity.User;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,5 +9,7 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
+    Optional<User> findByUsername(String username);
+    Optional<User> findByResetToken(String resetToken);
     boolean existsByEmail(String email);
 }
