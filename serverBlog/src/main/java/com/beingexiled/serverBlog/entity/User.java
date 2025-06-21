923edffbc6db0012286d664a16403074cd6a9111
nxstray <afwanapriansyah@gmail.com>
2025-06-22 00:16:58 +0700

    updating getter & setter without Data annotations: done

--- serverBlog/src/main/java/com/beingexiled/serverBlog/entity/User.java
+++ serverBlog/src/main/java/com/beingexiled/serverBlog/entity/User.java
@@ -1,15 +1,14 @@
 package com.beingexiled.serverBlog.entity;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
 import java.util.List;
 
-import jakarta.persistence.*;
-import lombok.Data;
-
 @Entity
-@Data
 @Table(name = "users")
 public class User {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -17,11 +16,84 @@ public class User {
     @Column(unique = true, nullable = false)
     private String email;
 
+    @Column(unique = true, nullable = false)
+    private String username;
+
     @Column(nullable = false)
     private String password;
-    
+
     private String role = "USER";
+
+    @Column(name = "reset_token")
+    private String resetToken;
+
+    @Column(name = "reset_token_created_at")
+    private LocalDateTime resetTokenCreatedAt;  
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Post> posts;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getResetToken() {
+        return resetToken;
+    }
+
+    public void setResetToken(String resetToken) {
+        this.resetToken = resetToken;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
+    }
+
+    public LocalDateTime getResetTokenCreatedAt() {
+        return resetTokenCreatedAt;
+    }
+
+    public void setResetTokenCreatedAt(LocalDateTime resetTokenCreatedAt) {
+        this.resetTokenCreatedAt = resetTokenCreatedAt;
+    }
 }
